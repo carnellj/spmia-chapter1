50b5fc75f5e512a10571afbe7db52128ceee5f6c
carnellj <john.carnell@inin.com>
2016-10-28 21:43:22 -0400

    CLeaned up the code

---
+++ src/main/docker/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+echo "********************************************************"
+echo "Starting simple-service ";
+echo "********************************************************"
+java -jar /usr/local/simple-service/simple-service-0.0.1-SNAPSHOT.jar
