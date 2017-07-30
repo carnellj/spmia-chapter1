f90dd8261d3f3e41fcd77ce7eda1eaee0e7507ca
carnellj <john.carnell@inin.com>
2017-07-30 10:22:38 -0400

    Working example of the Dockerfile Maven plugin

--- simpleservice/src/main/docker/run.sh
+++ simpleservice/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting simple-service "
 echo "********************************************************"
-java -jar /usr/local/simple-service/@project.build.finalName@.jar
+java -jar /usr/local/simple-service/simple-service-0.0.1-SNAPSHOT.jar
